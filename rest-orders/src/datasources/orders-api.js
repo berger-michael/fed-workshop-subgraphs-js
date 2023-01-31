ac3a093b8a7cbcb7aeba4142bc2b0a33c026420e
Michael Berger <41450229+berger-michael@users.noreply.github.com>
2023-01-31 11:00:24 -0700

    update 4

--- rest-orders/src/datasources/orders-api.js
+++ rest-orders/src/datasources/orders-api.js
@@ -2,9 +2,10 @@ const { RESTDataSource } = require("@apollo/datasource-rest");
 
 class OrdersAPI extends RESTDataSource {
   // @WORKSHOP 2.2.1: Apply the base URL here
-  baseURL = "";
+  baseURL = "https://rest-api-j3nprurqka-uc.a.run.app/api";
 
-  async getOrder() {
+  async getOrder(id) {
+    return this.get(`orders/${encodeURIComponent(id)}`);
     // @WORKSHOP 2.2.2: Make HTTP Get call to endpoint
   }
 }
